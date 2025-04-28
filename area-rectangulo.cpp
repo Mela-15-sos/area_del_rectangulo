a98111ef961cf7aab31f9929739b3fffd1f8b6b4
Mela-15-sos <sossmela037@gmail.com>
2025-04-28 09:02:11 -0600

    Add files via upload

---
+++ area-rectangulo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+int base,altura, area;
+
+std::cout<< "ingresa la base del resctangulo:";
+std::cin>> base;
+std::cout<< "ingresa la altura del resctangulo:";
+std::cin>> alrtura;
+area=base*altura;
+std::cout<<"el area del cuadrado es:"<<area;
+return 0;
+}
